5adebd8afab18acd99b7debeedddf2e873d1f4e7
Hallef Santos <hallefsantos@gmail.com>
2020-06-13 10:18:55 -0300

    Project 19 - Sortable List

--- table/app.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.leia-mais').addEventListener('click', (e) => {
-   const tableEl = e.target.parentElement;
-   if(tableEl.classList.contains('desc-esclarecimento')){
-      tableEl.classList.toggle('ativo');
-      e.target.innerHTML = 'Esconder texto';
-   }
-});
